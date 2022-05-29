078a8011d825d040c5240d6adebff263c7ca43c3
Ilya Shumilov <shumilovil@gmail.com>
2022-05-29 11:04:16 +0300

    draft server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,103 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const fs = require('fs');
 
 app.use(cors())
-app.get('/test', function (req, res) {
-    res.status(201).json({test: 'test'});
+app.use(express.json({extended: true}));
+
+
+app.get('/tasks', (req, res) => {
+    try {
+        fs.readFile('db.json', 'utf8', (err, data) => {
+            const taskList = JSON.parse(data).list;
+            res.status(201).json(taskList);
+        })
+
+    } catch (error) {
+        res.status(500).json({message: 'Something is wrong, try again'});
+    }
 })
+
+app.get('/tasks/:id', (req, res) => {
+    try {
+        fs.readFile('db.json', 'utf8', (err, data) => {
+            const taskList = JSON.parse(data).list;
+            const id = Number(req.params.id)
+            const task = taskList.find((task) => task.id === id)
+            res.status(201).json(task);
+        })
+
+    } catch (error) {
+        res.status(500).json({message: 'Something is wrong, try again'});
+    }
+})
+
+app.post('/tasks', (req, res) => {
+    try {
+        fs.readFile('db.json', 'utf8', (err, data) => {
+            const taskList = JSON.parse(data).list;
+            const sortedList = taskList.sort((a, b) => b.id - a.id)
+            const lastId = sortedList[0].id
+            taskList.push({id: lastId + 1, title: req.body.title, description: req.body.description})
+            const newJsonContent = JSON.stringify({list: taskList})
+            fs.writeFile('db.json', newJsonContent, (err) => {
+                if (err) return console.log(err);
+                res.status(201).json({message: 'added'});
+            });
+        })
+
+    } catch (error) {
+        res.status(500).json({message: 'Something is wrong, try again'});
+    }
+})
+
+app.patch('/tasks/:id', (req, res) => {
+    try {
+        fs.readFile('db.json', 'utf8', (err, data) => {
+            const taskList = JSON.parse(data).list;
+            const idToUpdate = Number(req.params.id)
+            const taskToUpdate = taskList.find(task => task.id === idToUpdate)
+            taskToUpdate.title = req.body.title
+            taskToUpdate.description = req.body.description
+
+            // taskList.push({id: lastId + 1, title: req.body.title, description: req.body.description})
+            const newJsonContent = JSON.stringify({list: taskList})
+            fs.writeFile('db.json', newJsonContent, (err) => {
+                if (err) return console.log(err);
+                res.status(201).json({message: 'added'});
+            });
+        })
+
+    } catch (error) {
+        res.status(500).json({message: 'Something is wrong, try again'});
+    }
+
+})
+
+app.delete('/tasks/:id', (req, res) => {
+    try {
+        fs.readFile('db.json', 'utf8', (err, data) => {
+            const taskList = JSON.parse(data).list;
+            const idToDelete = Number(req.params.id)
+            // const taskToUpdate = taskList.find(task => task.id === idToUpdate)
+            // taskToUpdate.title = req.body.title
+            // taskToUpdate.description = req.body.description
+            const taskListAfterDelete = taskList.filter((task) => task.id !== idToDelete)
+
+            // taskList.push({id: lastId + 1, title: req.body.title, description: req.body.description})
+            const newJsonContent = JSON.stringify({list: taskListAfterDelete})
+            fs.writeFile('db.json', newJsonContent, (err) => {
+                if (err) return console.log(err);
+                res.status(201).json({message: 'added'});
+            });
+        })
+
+    } catch (error) {
+        res.status(500).json({message: 'Something is wrong, try again'});
+    }
+})
+
 
 app.listen(5000, () => {
     console.log('CORS-enabled web server listening on port 5000')
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import superagent from 'superagent';
+// const superagent = require('superagent');
+
+
+// const axios = require('axios').default;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const handleClick = async () => {
+
+
+        const res = await superagent.delete('http://localhost:5000/tasks/10')
+        console.log(res)
+
+        return res
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <button onClick={handleClick}>Test</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
